eb04ea7e259b4a4997d2de7f6c16b74954a348a4
hammond022 <75580640+hammond022@users.noreply.github.com>
2025-11-12 04:48:00 +0800

    Added dedication message box during order creation

--- client/src/views/TransactionsView.vue
+++ client/src/views/TransactionsView.vue
@@ -561,6 +561,12 @@ onMounted(() => {
                 </div>
               </div>
             </div>
+            <div v-if="selectedTransaction?.dedicationMessage" class="mt-3">
+              <h6>Dedication Message:</h6>
+              <p class="fst-italic">
+                {{ selectedTransaction.dedicationMessage }}
+              </p>
+            </div>
           </div>
           <div class="modal-footer d-flex justify-content-between">
             <button
